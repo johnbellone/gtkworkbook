8f4cf8664eef8bce63fc351bc6e445a7dc384645
j_bellone <j_bellone@2d834452-db39-49f7-84f1-2edcd6a33ad8>
2009-04-02 04:31:59 +0000

    Added LICENSE and terms to all files.
    git-svn-id: https://gtkworkbook.svn.sourceforge.net/svnroot/gtkworkbook/trunk@4 2d834452-db39-49f7-84f1-2edcd6a33ad8

--- src/realtime/PacketParser.hpp
+++ src/realtime/PacketParser.hpp
@@ -1,3 +1,23 @@
+/* 
+   PacketParser.cpp - PacketParser Object Header File
+
+   The GTKWorkbook Project <http://gtkworkbook.sourceforge.net/>
+   Copyright (C) 2008, 2009 John Bellone, Jr. <jvb4@njit.edu>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PRACTICAL PURPOSE. See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301 USA
+*/
 #ifndef HPP_PACKETHANDLER
 #define HPP_PACKETHANDLER
 
@@ -7,19 +27,19 @@
 #include <workbook/sheet.h>
 #include <workbook/workbook.h>
 
-class PacketHandler : public proactor::Job {
+class PacketParser : public proactor::Job {
 private:
   Workbook * wb;
   FILE * pktlog;
   Cell * cell;
 public:
-  PacketHandler (Workbook * wb, FILE * pktlog) { 
+  PacketParser (Workbook * wb, FILE * pktlog) { 
     this->wb = wb;
     this->pktlog = pktlog;
     this->cell = cell_new();
   }
   
-  virtual ~PacketHandler (void) {
+  virtual ~PacketParser (void) {
     cell->destroy (cell);
   }
 
