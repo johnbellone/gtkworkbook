f9001ee4fe972e2c543aec69db7eef58ddf9e043
John Bellone <jvb4@njit.edu>
2009-09-09 11:36:42 -0400

    Updated documentation info

--- etc/yahooQuoteServer.py
+++ etc/yahooQuoteServer.py
@@ -44,18 +44,3 @@ def main():
 # Execute the main method of the script.
 if __name__ == "__main__":
     main()
-
-# Copyright (C) 2009, John Bellone, Jr. <jvb4@njit.edu>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy olf the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
